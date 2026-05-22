0c679791412b34bb8c84cdb82fbda28c41580107
Ganes769 <ganeshsnawali@gmail.com>
2026-05-22 15:52:48 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-# def slugify(text=str, sep="`*"):
-#     clean = text.strip().lower()
-#     return clean.replace("-", sep)
+def slugify(text: str, sep="`*"):
+    clean = text.strip().lower()
+    return clean.replace("-", sep)
 
 
-# print(slugify("payment-api", sep="8"))
+print(slugify("payment-api", sep="8"))
+
+
+# using * in the func arg
+def archive_project(name: str, *, notify=False, force=True):
+    if force:
+        print(f"Force archive {name}")
+    if notify:
+        print(f"Notify archive {name}")
+
+
+print(archive_project("nasa", notify=True, force=False))
+
+
+def build_project(name, **kwargs):
+    # With kwargs we can give multiple argument for function signature
+    project = {"name": name, "slug": slugify(name)}
+    project.update(kwargs)
+    return project
+
+
+print(build_project("Nasa", description="description"))
